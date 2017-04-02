45e9803f9186b1c24307834ca3d98308e5415fd8
Patrick Hulce <patrick.hulce@gmail.com>
2017-04-02 13:01:44 -0700

    feat: add react

---
+++ test/fixtures/react/file.js
@@ -0,0 +1,28 @@
+import dep from './src/dep'
+import extDep from 'src/dep2'
+import unused from 'path'
+import React, {PropTypes} from 'react'
+
+export default React.createClass({
+  propTypes: {
+    children: PropTypes.element,
+    unused: PropTypes.number,
+  },
+  renderOther() {
+    return <a className="foobar">Hey</a>;
+  },
+  render() {
+    return (
+      <div className='container'
+      onClick={dep}
+      onBlur={this.renderOther}>
+    <h1>Home</h1>
+        <Link to="contact">Contact</Link>
+              {this.props.children}
+      </div>
+    )
+  },
+render2() {
+  extDep()
+},
+})
