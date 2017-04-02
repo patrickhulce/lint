217ec0704ff668d0688fe803635b749756834645
Patrick Hulce <patrick.hulce@gmail.com>
2017-04-02 15:28:16 -0700

    refactor: extract logic to lib

--- bin/lint.js
+++ bin/lint.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
-const path = require('path')
 const yargs = require('yargs')
-const CLIEngine = require('eslint').CLIEngine
-const formatter = require('eslint-formatter-pretty')
+const lint = require('../lib/lint.js')
 
 const argv = yargs
   .usage('Usage: $0 [--fix] [path]')
@@ -12,33 +10,4 @@ const argv = yargs
   })
   .argv
 
-let pkgConfig = {}
-try {
-  const pkg = require(path.join(process.cwd(), 'package.json'))
-  pkgConfig = pkg.eslint || (pkg.config && pkg.config.eslint)
-} catch (err) {
-  process.stderr.write(err.stack)
-}
-
-let configFile = 'eslint.config.js'
-configFile = path.resolve(__dirname, `../lib/${configFile}`)
-
-const config = Object.assign({
-  fix: argv.fix,
-  ignorePattern: './**/*.test.js',
-  configFile,
-}, pkgConfig)
-
-const paths = argv._
-if (paths.length === 0) {
-  paths.push('./+(lib|bin|src)/**/*.js', './*.js')
-}
-
-const engine = new CLIEngine(config)
-const report = engine.executeOnFiles(argv._)
-if (argv.fix) {
-  CLIEngine.outputFixes(report)
-}
-
-process.stdout.write(formatter(report.results))
-process.exit(report.errorCount === 0 ? 0 : 1)
+lint(argv)
